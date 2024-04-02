b01da485afa165992267b3c3c9f6aef65d9354a3
cotswoldsmaker <mark.allan.bailey@gmail.com>
2024-04-02 21:40:19 +0100

    Update license and README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learn-python
-This course for anyone in the healthcare space who wants to code. The main programming language is python.
+
+This is course for clinicians who want to code. The main programming language is python.
+
+[Module web view](https://clinicians-who-code.github.io/learn-python/)
---
+++ users/anita_phung/module-1/hands-on/lesson_1.py
@@ -0,0 +1,36 @@
+""" Lesson 1 
+Some basic terminal and python exercises
+Gotten stuck, look at the slides at 
+"""
+
+"""Exercise 1
+Just run this python script
+"""
+
+
+"""Exercise 2
+Change the output from the print function to print your name.
+"""
+
+print("Hello world!")
+
+
+"""Exercise 3
+Run a terminal command to show all files and folders in your current directory.
+"""
+
+
+"""Exercise 4
+1. Run a terminal command to take you back one level in the directory path.
+2. Run a terminal command to take you back to where you started.
+"""
+
+
+"""Exercise 4
+Run a terminal command to show what is your current directory path.
+"""
+
+"""Finished
+Let your tutor know when you have finished the above. We will restart go to
+next session when everyone is finished with this exercise.
+"""
